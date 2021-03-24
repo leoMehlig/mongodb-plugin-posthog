692d0bf38c3c34e07a7d278778801d251e66bc65
leoMehlig <leo@leomehlig.com>
2021-03-24 13:12:44 +0100

    Bundle

---
+++ rollup.config.js
@@ -0,0 +1,60 @@
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import pkg from './package.json';
+// import json from "@rollup/plugin-json";
+
+
+// export default [
+// 	// browser-friendly UMD build
+// 	{
+// 		input: 'src/main.js',
+//         output: {
+//             file: 'dist/index.js',
+//             format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+//             sourcemap: true
+//         },
+// 		plugins: [
+// 			resolve(), // so Rollup can find `ms`
+// 			commonjs(), // so Rollup can convert `ms` to an ES module
+//             json()
+// 		]
+// 	},
+
+// 	// CommonJS (for Node) and ES module (for bundlers) build.
+// 	// (We could have three entries in the configuration array
+// 	// instead of two, but it's quicker to generate multiple
+// 	// builds from a single configuration where possible, using
+// 	// an array for the `output` option, where we can specify
+// 	// `file` and `format` for each target)
+// 	{
+// 		input: 'src/main.js',
+// 		external: ['ms'],
+// 		output: [
+// 			{ file: pkg.main, format: 'cjs' },
+// 			{ file: pkg.module, format: 'es' }
+// 		]
+// 	}
+// ];
+
+
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from "@rollup/plugin-json";
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	input: 'src/main.js',
+	output: {
+		file: 'public/bundle.js',
+		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+		sourcemap: true
+	},
+	plugins: [
+		resolve(), // tells Rollup how to find date-fns in node_modules
+		commonjs(), // converts date-fns to ES modules
+        json() // minify, but only in production
+	]
+};
---
+++ src/index.js
@@ -0,0 +1,78 @@
+const MongoClient = require('mongodb').MongoClient;
+
+var groupBy = function(xs, key) {
+    return xs.reduce(function(rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+};
+
+async function setupPlugin({ global, attachments, config }) {
+    if (!config.databaseUrl) {
+        throw new Error('Database Url not provided!')
+    }
+    if (!config.databaseName) {
+        throw new Error('Database Name not provided!')
+    }
+
+    const client = await MongoClient.connect(config.databaseUrl, { useNewUrlParser: true })
+        .catch(err => { console.log(err); });
+
+    if (!client) {
+        throw new Error('Failed to setup client')
+    }
+
+    global.database = client.db(config.databaseName);
+}
+
+async function processEventBatch(batch, { config, global }) {
+    if (!global.database) {
+        throw new Error('No database initialized!')
+    }
+
+    const rows = batch.map((oneEvent) => {
+        const { event, properties, $set, $set_once, distinct_id, team_id, site_url, now, sent_at, uuid, ..._discard } = oneEvent
+        const ip = properties?.['$ip'] || oneEvent.ip
+        const timestamp = oneEvent.timestamp || oneEvent.data?.timestamp || properties?.timestamp || now || sent_at
+        let ingestedProperties = properties
+        let elements = []
+
+        // only move prop to elements for the $autocapture action
+        if (event === '$autocapture' && properties['$elements']) {
+            const { $elements, ...props } = properties
+            ingestedProperties = props
+            elements = $elements
+        }
+
+        return {
+            uuid,
+            event,
+            properties: JSON.stringify(ingestedProperties || {}),
+            elements: JSON.stringify(elements || {}),
+            set: JSON.stringify($set || {}),
+            set_once: JSON.stringify($set_once || {}),
+            distinct_id,
+            team_id,
+            ip,
+            site_url,
+            timestamp: timestamp ? global.bigQueryClient.timestamp(timestamp) : null,
+        }
+    })
+
+
+    var events = groupBy(ros, "event")
+
+    try {
+        for (var event in events) {
+            var col = global.database.collection(event)
+            await col.insertMany(events[event], { ordered: false })
+        }
+    } catch (error) {
+        throw new Error(`Error inserting into mongodb! ${JSON.stringify(error.errors)}`)
+    }
+
+    return batch
+}
+
+module.exports.setupPlugin = setupPlugin;
+module.exports.processEventBatch = processEventBatch;
